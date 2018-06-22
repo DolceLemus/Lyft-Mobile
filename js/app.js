c15f5861082cc39ee5c2ebd224d262145252fd37
DolceLemus <dulce.lemus.lara@gmail.com>
2018-06-22 10:46:09 -0500

    se genera codigo aleatorio

--- js/app.js
+++ js/app.js
@@ -5,3 +5,24 @@ $(document).ready(function() {
         $("#main-index").show();
     }, 5000);
 });
+
+//src=https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Math/random
+// random numbers
+
+function genNmber() {
+    var newNumber = "";
+    // function getRandomArbitrary(min, max) {
+    //   return Math.random() * (max - min) + min;
+    // }
+    for (var i = 0; i < 3; i++) {
+        var codeAscii = Math.floor(Math.random() * (57 - 48) + 48);
+        var stringCodeAscii = String.fromCharCode(codeAscii);
+        newNumber+=stringCodeAscii;
+    }
+
+    alert(newNumber);
+}
+
+$(document).ready(function() {
+    $("#btn-next-asknumber").click(genNmber);
+});
